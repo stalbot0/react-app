6b651849e3bc873ac00040e0406694fb221b745a
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-05-25 20:52:28 -0500

    separated components in react

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,14 @@
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.99;
 
   return (
-    <div className='expense-item'>
-      <div>{expenseDate.toDateString()}</div>
-      <div className='expense-item__description'>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
