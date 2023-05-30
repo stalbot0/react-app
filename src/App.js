a783e32c3a89449bf01aec0e1ec96d5fffa58674
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-05-30 10:34:34 -0500

    added user input, need to pull values from user input next

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 import React from "react";
 
 const App = () => {
@@ -32,6 +33,7 @@ const App = () => {
 
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
