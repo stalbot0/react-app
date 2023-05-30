d279674265f471e514f7d4877610eeb3263d1f32
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-05-30 10:20:16 -0500

    working with React state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,24 @@
+import React, { useState } from 'react';
+// useState allows us to define values as state where changes to the values are reflected within the js
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  
+
+  const [title, setTitle] = useState(props.title); //useState always returns an array length 2 [0] is the old value, [1] is the updated function to return the new value
+
   const clickHandler = () => {
-    console.log("Clicked!!");  
+    setTitle('Updated!');
+    console.log(title);
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
