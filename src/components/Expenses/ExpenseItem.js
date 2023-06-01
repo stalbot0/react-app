79a2d4f0ed6e956a49c976d9f51c44ff37b78999
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-06-01 10:47:18 -0500

    changed the state of the expense item - moving onto lists and conditional content

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,21 +7,21 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
 
-  const [title, setTitle] = useState(props.title); //useState always returns an array length 2 [0] is the old value, [1] is the updated function to return the new value
+  // const [title, setTitle] = useState(props.title); //useState always returns an array length 2 [0] is the old value, [1] is the updated function to return the new value
 
-  const clickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
-  }
+  // const clickHandler = () => {
+  //   setTitle('Updated!');
+  //   console.log(title);
+  // }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 };
