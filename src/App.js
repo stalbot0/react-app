97b75a7c5eeb1bec3e61ca8fb165ad0c3e2200bf
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-06-12 12:01:44 -0500

    unique keys added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-import React from "react";
+import React, { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  // uses destructuring to create an array of objects that are expenses, along with a fxn that allows us to alter the state of the array. 
+  // The useState initial state value are the DUMMY_EXPENSES
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("in App.js");
-    console.log(expense);
+    // we're setting our new expense to the first element in the NEW array, and then using the spread operation for the rest of the expenses
+    //  following the newly created expense. the PrevExpenses fxn will update our expenses based on a snapshot of our previously used expenses
+    setExpenses((prevExpenses) => {return [expense, ...prevExpenses]});
   };
 
   // return React.createElement(
